8852b82973700af850ff98b348bd0227423a0ef1
Zeusina <kachusov_k@outlook.com>
2023-01-27 22:06:30 +0300

    Added class to work with sample text and added a sample text

--- utils.py
+++ utils.py
@@ -1,22 +1,19 @@
-import logging
 import configparser
-from aiogram import Dispatcher
-from aiogram.contrib.middlewares.i18n import I18nMiddleware
-from pathlib import Path
-
+import json
+import logging
 
 
 class ConfigUtils:
     def config_parse(self):
         config = configparser.ConfigParser()
-        config.read("config.ini")
+        config.read("config.ini", encoding="utf-8")
         return config
 
 
 class LoggingUtils:
     def __init__(self, name: str = "Bot"):
         config = ConfigUtils().config_parse()
-        logging.basicConfig(level=config["LOG"]["level"])
+        logging.basicConfig(level=config["LOG"]["level"], format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
         ll = logging.getLogger(name)
         ll.setLevel(config["LOG"]["level"])
         self.log = ll
@@ -25,9 +22,20 @@ class LoggingUtils:
         return self.log
 
 
-log = LoggingUtils("Utils").log
+log = LoggingUtils("utils").log
 
 
 class TelegramUtils:
     def __init__(self):
         log.info("Telegram utils initialized!")
+
+
+class SampleUtils:
+    def __init__(self):
+        log.info("Sample utils initialized!")
+        self.config = ConfigUtils().config_parse()
+
+    def get_sample(self):
+        with open(self.config['SAMPLE']['name'].strip() + self.config['SAMPLE']['ext'].strip(), "r", encoding="utf-8") as f:
+            data = json.load(f)
+        return data['post_text']
