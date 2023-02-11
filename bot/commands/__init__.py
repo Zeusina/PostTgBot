c0114a9f054112570e1cd61f655c3cad9df16895
Zeusina <kachusov_k@outlook.com>
2023-02-11 17:46:05 +0300

    Added logging in commands/__init__.py

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -2,7 +2,11 @@ from aiogram import Router
 from aiogram.filters.command import CommandStart
 
 from bot.commands.start import start_command
+import logging
+
+logger = logging.getLogger('commands/__init__.py')
 
 
 def register_user_commands(router: Router) -> None:
     router.message.register(start_command, CommandStart())
+    logger.info("User commands registered")
