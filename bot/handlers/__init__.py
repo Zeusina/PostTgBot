ddcd3856761ac71103bc2343c207f75f37f21709
Zeusina <kachusov_k@outlook.com>
2023-02-12 09:31:58 +0300

    Add_post works now

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -3,11 +3,13 @@ import logging
 from aiogram import Router
 from aiogram.filters.command import CommandStart, Command
 
-from bot.handlers.add_post import *
+from bot.handlers.add_post import (cancel_add_post, add_post_command, add_post_number, add_post_name,
+                                   add_post_words_count, add_post_file, add_post_check, add_post_send)
 from bot.handlers.start import start_command
+from bot.handlers.bot_commands import bot_commands
 from bot.structures.fsm_groups import AddPostStates
 
-__all__ = ["start_command"]
+__all__ = ["start_command", "bot_commands", ]
 
 logger = logging.getLogger('commands/__init__.py')
 
@@ -21,5 +23,5 @@ def register_user_commands(router: Router) -> None:
     router.message.register(add_post_words_count, AddPostStates.waiting_for_words_count)
     router.message.register(add_post_file, AddPostStates.waiting_for_file)
     router.message.register(add_post_check, AddPostStates.waiting_for_channel)
-    router.message.register(add_post_send, AddPostStates.waiting_for_send)
+    router.message.register(add_post_send, AddPostStates.waiting_for_send, Command(commands=['send']))
     logger.info("User commands registered")
--- bot/handlers/add_post.py
+++ bot/handlers/add_post.py
@@ -1,8 +1,10 @@
 from aiogram import types
 from aiogram.fsm.context import FSMContext
+from aiogram.methods import SendAudio
+from aiogram.types import ReplyKeyboardRemove
 
 from ..structures.fsm_groups import AddPostStates
-from bot.structures.keyboards import add_post_keyboard
+from bot.structures.keyboards.add_post_keyboard import ADD_POST_BOARD_CHANNEL
 
 
 async def add_post_command(message: types.Message, state: FSMContext) -> None:
@@ -36,24 +38,35 @@ async def add_post_words_count(message: types.Message, state: FSMContext) -> Non
 
 async def add_post_file(message: types.Message, state: FSMContext) -> None:
     if message.audio:
-        keyboard = add_post_keyboard
-        await message.answer("Пожалуйста отправьте канал для публикации", reply_markup=keyboard)
+        await message.answer("Пожалуйста отправьте канал для публикации", reply_markup=ADD_POST_BOARD_CHANNEL)
         await state.set_state(AddPostStates.waiting_for_channel)
-        await state.update_data(file_id=message.audio.file_unique_id)
+        await state.update_data(file_id=message.audio.file_id)
     else:
         await message.answer("Пожалуйста отправьте музыкальный файл изложения")
 
 
 async def add_post_check(message: types.Message, state: FSMContext) -> None:
-    await message.answer("Пожалуйста проверьте получившийся пост.")
+    chat = message.chat_shared.chat_id
+    data = await state.get_data()
+    await state.update_data(channel=chat)
+    msg = "Название изложения: <code>%name%</code>\n".replace("%name%", data.get("name"))
+    msg += "Номер изложения: <code>%number%</code>\n".replace("%number%", data.get("number"))
+    msg += "Количество слов: <code>%words_count%</code>".replace("%words_count%", data.get("words_count"))
+    await message.answer("Пожалуйста проверьте получившийся пост.", reply_markup=ReplyKeyboardRemove())
+    await message.answer_audio(caption=msg, parse_mode="HTML", audio=data.get("file_id"))
+    await message.answer("Если все верно, то отправьте /send, иначе /cancel")
     await state.set_state(AddPostStates.waiting_for_send)
+    await state.update_data(msg=msg)
 
 
 async def add_post_send(message: types.Message, state: FSMContext) -> None:
+    data = await state.get_data()
+    await SendAudio(chat_id=data.get("channel"), caption=data.get("msg"), parse_mode="HTML",
+                    audio=data.get("file_id"))
     await message.answer("Пост отправлен.")
     await state.clear()
 
 
 async def cancel_add_post(message: types.Message, state: FSMContext) -> None:
-    await message.answer("Отменено")
+    await message.answer("Отменено", reply_markup=ReplyKeyboardRemove())
     await state.clear()
