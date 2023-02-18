e053570d9ccc8685acb1bbf8c604acecc63bc363
Zeusina <kachusov_k@outlook.com>
2023-02-18 17:57:26 +0300

    Fixed send chat button text

--- bot/structures/keyboards/add_post_keyboard.py
+++ bot/structures/keyboards/add_post_keyboard.py
@@ -3,7 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, KeyboardButtonReq
 from bot.structures.get_chat_administrator_rights import get_administrator_rights
 
 ADD_POST_BOARD_CHANNEL = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, keyboard=[
-    [KeyboardButton(text="Проверить", request_chat=KeyboardButtonRequestChat(
+    [KeyboardButton(text="Отправить канал", request_chat=KeyboardButtonRequestChat(
         request_id=1,
         chat_is_channel=True,
         user_administrator_rights=get_administrator_rights(can_post_messages=True, can_edit_messages=True,),
